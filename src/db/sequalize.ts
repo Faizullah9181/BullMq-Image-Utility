0abd0e18a548dac59f3d0bb1c05bae2268dded52
faizullah9181@gmail.com <faizullah@gmail.com>
2024-02-13 10:04:06 +0000

    cleanup@

--- src/db/sequalize.ts
+++ src/db/sequalize.ts
@@ -94,11 +94,3 @@ export const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   options
 );
-
-console.log(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  process.env.DB_HOST,
-  process.env.DB_PORT
-);
